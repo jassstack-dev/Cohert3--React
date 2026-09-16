dd08852a8b4edf39bc70f26d8c0fd4877dabdde4
jass-vimal <vimaljass8@gmail.com>
2026-09-16 11:12:18 +0530

    understand jsx, components and props

---
+++ day2-jsx_comp/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import About from './About'
+
+const App = () => {
+  return (
+    <div>
+      <h1>HEllo bhai </h1>
+      <About  width = "hello bhai"/>    
+    </div>
+
+
+    // ___________________JSX___________________________
+    // jsx ek javascript hai and ye behind the scene div ko babel ke through javascript mai change karti hai then javascript html code ko samajh pata  hai babel and complier hai to html code ko javascript code mai change karti  hai 
+
+    // _________________________Components_________________________
+    //  =====> components mtlb functions ==> component ek block of code hai jo tukdo mai ui banake deta  hai
+    //___________Rules of Components
+    // 1) file ka first letter bada hona chahiye
+    // 2) and function ka bhi pahala letter bada hona chhaiye
+    // 3) call elements jaise honge  
+
+
+    // props => mtlb properties 
+    // agar hame jsx file mai javascript ko run karna hai to curly bracket lagane honge and then javascript a code usme daaloge then code chalega 
+
+    // ________________ and hamesha yaad rakhna return ke upar javascript code chalta hai_______
+
+
+
+
+    
+  )
+}
+
+export default App
